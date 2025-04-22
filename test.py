c72c14926f1c3cef2b52ffdef2a6534b2c0b7a39
nevrohelios <dasharka05@gmail.com>
2025-04-23 00:21:39 +0530

    [FEAT] Hybrid query working

--- test.py
+++ test.py
@@ -1,35 +1,35 @@
-import os
-
 from colorama import Fore
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain_community.document_loaders import UnstructuredMarkdownLoader
-from langchain_community.vectorstores import Qdrant
 from langchain_huggingface import HuggingFaceEmbeddings
+from langchain_qdrant import Qdrant
 from qdrant_client import QdrantClient
 from qdrant_client.models import Distance, VectorParams
 
 MARKDOWN_FILE_PATH = "data/gradingdoc.md"
 QDRANT_COLLECTION_NAME = "qdrant_index"
 EMBEDDING_MODEL_NAME = "BAAI/bge-small-en-v1.5"  # "all-MiniLM-L6-v2"
+QDRANT_HOST = "localhost"
+QDRANT_PORT = 6333
 
 
 def load_or_create_vectore_store(vectore_store=None):
     embeddings = HuggingFaceEmbeddings(model_name=EMBEDDING_MODEL_NAME)
 
-    client = QdrantClient(host="localhost", port=6333)
+    client = QdrantClient(host=QDRANT_HOST, port=QDRANT_PORT)
 
-    if QDRANT_COLLECTION_NAME not in [
-        c.name for c in client.get_collections().collections
-    ]:
+    if not client.collection_exists(collection_name=QDRANT_COLLECTION_NAME):
+
         loader = UnstructuredMarkdownLoader(MARKDOWN_FILE_PATH)
         documents = loader.load()
-        text_splitter = RecursiveCharacterTextSplitter(chunk_size=100, chunk_overlap=10)
+        text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=300)
         docs = text_splitter.split_documents(documents)
         print(
             Fore.LIGHTBLUE_EX + f"Split document into {len(docs)} chunks." + Fore.RESET
         )
 
         # Create collection
+
         client.recreate_collection(
             collection_name=QDRANT_COLLECTION_NAME,
             vectors_config=VectorParams(
@@ -38,11 +38,13 @@ def load_or_create_vectore_store(vectore_store=None):
         )
 
         vector_store = Qdrant.from_documents(
+            documents=docs,
             embedding=embeddings,
             collection_name=QDRANT_COLLECTION_NAME,
-            client=client,
+            host=QDRANT_HOST,
+            port=QDRANT_PORT,
         )
-        vector_store.add_documents(docs)
+
         print(
             Fore.LIGHTGREEN_EX
             + f"Saved Qdrant collection: {QDRANT_COLLECTION_NAME}"
@@ -57,11 +59,23 @@ def load_or_create_vectore_store(vectore_store=None):
     return vector_store
 
 
+def query_documents(query: str) -> str:
+    vector_store = load_or_create_vectore_store()
+    retriever = vector_store.as_retriever(search_kwargs={"k": 3})
+    results = ""
+
+    print(Fore.YELLOW + f"Found {len(results)} relevant documents." + Fore.RESET)
+    for result in retriever.invoke(query):
+        results += f"Document: {result.page_content}\n\n"
+
+    return results
+    
+
 if __name__ == "__main__":
     vector_store = load_or_create_vectore_store()
     retriever = vector_store.as_retriever(search_kwargs={"k": 3})
     results = retriever.invoke(
-        "What is the grading policy for Application Development - 2 (Diploma in programming)?"
+        "what are the eligibilities for the final exams?"
     )
 
     print(Fore.YELLOW + f"Found {len(results)} relevant documents." + Fore.RESET)
