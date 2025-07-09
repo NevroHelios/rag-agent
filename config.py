d6efb63efee74f9a7fb02c931a5b17365527fef3
boosboy <drakesh2107@gmail.com>
2025-07-09 11:51:07 +0530

    Add import for ChatOpenAI and update comments in configuration

--- config.py
+++ config.py
@@ -1,6 +1,7 @@
 import os
 from langchain.graphs.neo4j_graph import Neo4jGraph
 from langchain_ollama.chat_models import ChatOllama
+from langchain_openai import ChatOpenAI
 from langchain_ollama import OllamaEmbeddings
 from dotenv import load_dotenv
 
@@ -19,6 +20,14 @@ class CFG:
         model="llama3.1:8b",
         temperature=0,
     )
+
+    # llm = ChatOpenAI(
+    #     model="gpt-4o-mini",
+    #     temperature=0,
+    #     max_tokens=1000,
+    #     streaming=True,
+    # )
+
     # embedding model
     embedding_model = OllamaEmbeddings(model="nomic-embed-text:latest")
     
